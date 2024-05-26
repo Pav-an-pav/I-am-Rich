82e95dafa5548ee2d7b0e3fc856dca8f1ef519bb
Pav-an-pav <pav-an-pav@yandex.com>
2024-05-26 12:18:29 +0500

    First

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Anna Pavlova on 21.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
